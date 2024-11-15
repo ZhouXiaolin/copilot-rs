20e69ce573b8ebd63344563de3c61044687051c1
zhouxl <zhouxl@ckt.cn>
2024-11-15 17:20:07 +0800

    refactor(src): update client function and test
    - Refactor client function to use ChatModel builder
    - Update test function to use new client
    - Remove unused imports and comments

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,23 @@ extern crate copilot_rs;
 fn main() {
     let a = test("深圳");
     println!("{a}");
-
 }
 
-
 fn client() -> copilot_rs::ChatModel {
-    let config = include_str!("../config.json");
-    serde_json::from_str(config).unwrap()
+    // let config = include_str!("../config.json");
+    // serde_json::from_str(config).unwrap()
+    let chat_model = copilot_rs::ChatModel::builder()
+        .chat_api_base("chat_api_base".to_string())
+        .chat_api_key("chat_api_key".to_string())
+        .chat_model_default("chat_model_default".to_string())
+        .build();
+    chat_model
 }
 // complete会将函数体和参数注入到函数中
 #[complete(client="client", temperature=0.6, max_tokens=1000, tools=["Add", "GetCurrentWeather"])]
 fn test(name: &str) -> String {
-    vec![format!("{}今天天气怎么样",name).user()].chat()
+    vec![format!("{}今天天气怎么样", name).user()].chat()
 }
-
 
 #[derive(FunctionTool, Deserialize, Serialize)]
 #[props(desc = "Get weather of an location, the user shoud supply a location first")]
@@ -32,7 +35,6 @@ impl FunctiomImplTrait for GetCurrentWeather {
         "艳阳天".to_string()
     }
 }
-
 
 #[derive(FunctionTool, Deserialize, Serialize)]
 #[props(desc = "add two number")]
